8fa369b7ff5b8f086fd6cf48710a045604ab8e8c
SweetMustard993 <bogdanasdaq@gmail.com>
2022-02-20 12:42:48 +0300

    3. Интерфейс Input [#181092 #267124]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,19 +1,33 @@
 package ru.job4j.poly;
 
+import java.util.Scanner;
+
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        Scanner input =new Scanner(System.in);
+        boolean stop = false;
+        while (!stop){
+            System.out.println("водитель совершает остановку по требованию");
+            String busStop = input.nextLine();
+            if (busStop == null){
+                System.out.println("автобус проезжает без остановки");
+            } else {
+                System.out.println("автобус совершит остановку на" + busStop);
+                stop = true;
+            }
+        }
     }
 
     @Override
     public void passanger(int passangersCount) {
-
+        int allSeats = 20;
+        int freeSeats = allSeats - passangersCount;
     }
 
     @Override
     public double refuel(double fuelVolume) {
-        double cost = 0;
+        double cost = fuelVolume * 52.89;
         return cost;
     }
 }
