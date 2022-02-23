80f36232590c0b9851f2f6e0ae3c74969fe9a53b
SweetMustard993 <bogdanasdaq@gmail.com>
2022-02-23 12:11:33 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #268370]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -35,7 +35,7 @@ public class StartUI {
 
     public static void editItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
-        int id = Integer.parseInt(input.askStr("Enter id: "));
+        int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
