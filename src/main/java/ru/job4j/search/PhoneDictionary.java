668e362bfb6146f1d8db2139a4926cc69277c137
SweetMustard993 <bogdanasdaq@gmail.com>
2022-03-30 13:29:35 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #281959]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -12,7 +12,8 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.toString().contains(key)) {
+            if (person.getAddress().contains(key) || person.getPhone().contains(key)
+                    || person.getName().contains(key) || person.getSurname().contains(key)) {
                 result.add(person);
             }
         }
