ac7d04bc4cd34966892f78615b14796c47cca7b7
SweetMustard993 <bogdanasdaq@gmail.com>
2022-04-24 16:47:58 +0300

    2. Отсортировать департаменты [#34352 #292493]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -1,19 +1,17 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int rsl = 0;
         int length = Math.min(o1.length(), o2.length());
-        for (int i = 0; i < length; i++) {
-            rsl = i > 1 ? Character.compare(o1.charAt(i), o2.charAt(i))
-                    : Character.compare(o2.charAt(i), o1.charAt(i));
-            if (rsl != 0) {
-                break;
-            }
-        }
-        return rsl == 0 ? Integer.compare(o1.length(), o2.length()) : rsl;
+        List<String> o1List = Arrays.asList(o1.split("/"));
+        List<String> o2List = Arrays.asList(o2.split("/"));
+        int rsl = o2List.get(0).compareTo(o1List.get(0));
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -18,8 +18,7 @@ public class Departments {
     }
 
     public static void sortAsc(List<String> orgs) {
-        Comparator<String> asc = new DepAscComp();
-        Collections.sort(orgs, asc);
+        Collections.sort(orgs);
     }
 
     public static void sortDesc(List<String> orgs) {
